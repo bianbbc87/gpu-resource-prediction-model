4b05905f03c2c8efd34d87ddeefc126067fe17cd
EunJiJung <bianbbc87@gmail.com>
2026-01-02 00:51:23 +0900

    refactor: gcs config 기준 util 수정
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,25 @@
 import yaml
+from google.cloud import storage
 
-def load_config(path):
-    with open(path) as f:
-        return yaml.safe_load(f)
+def load_config(path: str):
+    """
+    Load YAML config from local path or GCS (gs://).
+    """
+    if path.startswith("gs://"):
+        # gs://bucket/path/to/file.yaml
+        bucket_name, blob_path = path[5:].split("/", 1)
+
+        client = storage.Client()
+        bucket = client.bucket(bucket_name)
+        blob = bucket.blob(blob_path)
+
+        if not blob.exists():
+            raise FileNotFoundError(f"GCS config not found: {path}")
+
+        content = blob.download_as_text()
+        return yaml.safe_load(content)
+
+    else:
+        # local file
+        with open(path, "r") as f:
+            return yaml.safe_load(f)
